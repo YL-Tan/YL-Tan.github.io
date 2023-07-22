45fa4bfe1d03194c3644777173bd98be429dcdd6
YL-Tan <77035911+YL-Tan@users.noreply.github.com>
2023-07-22 14:29:54 -0700

    Added the animation code for timeline

--- app.js
+++ app.js
@@ -4,6 +4,33 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 const themeBtn = document.querySelector('.theme-btn');
 
+themeBtn.addEventListener('click', () => {
+  document.body.classList.toggle("light-mode");
+  if (document.body.classList.contains('light-mode')) {
+      themeBtn.setAttribute('data-tooltip', 'Light Mode');
+  } else {
+      themeBtn.setAttribute('data-tooltip', 'Dark Mode');
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  let timelineItems = Array.from(document.querySelectorAll('.timeline-item'));
+
+  let observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+      } else {
+        entry.target.classList.remove('in-view');
+      }
+    });
+  }, {threshold: 0.1});
+
+  timelineItems.forEach(item => {
+    observer.observe(item);
+  });
+});
+
 // Event Delegation
 controls.addEventListener('click', function(e){
     const btn = e.target.closest('.control');
@@ -38,12 +65,3 @@ if (sections && controls && sectBtn && allSections) {
 } else {
     console.error('Some DOM selections failed');
 }
-
-themeBtn.addEventListener('click', () => {
-    document.body.classList.toggle("light-mode");
-    if (document.body.classList.contains('light-mode')) {
-        themeBtn.setAttribute('data-tooltip', 'Light Mode');
-    } else {
-        themeBtn.setAttribute('data-tooltip', 'Dark Mode');
-    }
-});
